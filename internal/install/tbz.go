dc58058ca4afae4b82179e45e7688b01fda897a9
Anton Patsev <10828883+patsevanton@users.noreply.github.com>
2021-10-13 19:03:04 +0600

    WIP Support tbz

---
+++ internal/install/tbz.go
@@ -0,0 +1,73 @@
+package install
+
+import (
+	"archive/tar"
+	"bytes"
+  "compress/bzip2"
+	"io"
+	"io/ioutil"
+	"log"
+	"os"
+
+
+	"github.com/devops-works/binenv/internal/mapping"
+	"github.com/devops-works/binenv/internal/tpl"
+)
+
+// TarBZIP2 handles bzip2 files
+type TarBZIP2 struct {
+	filters []string
+}
+
+// Install file from bzip2 file
+func (x TarBZIP2) Install(src, dst, version string, mapper mapping.Mapper) error {
+	noMatches := ErrNoMatch
+
+	data, err := ioutil.ReadFile(src)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	r, err := bzip2.NewReader(bytes.NewReader(data), 0)
+	if err != nil {
+		return err
+	}
+
+	tarReader := tar.NewReader(r)
+	args := tpl.New(version, mapper)
+
+	for true {
+		header, err := tarReader.Next()
+		if err == io.EOF {
+			break
+		}
+
+		if err != nil {
+			return err
+		}
+
+		switch header.Typeflag {
+		case tar.TypeReg: // regular file
+			ok, err := args.MatchFilters(header.Name, x.filters)
+			if err != nil {
+				return err
+			}
+			if !ok {
+				continue
+			}
+
+			out, err := os.OpenFile(dst, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0750)
+			if err != nil {
+				return err
+			}
+			defer out.Close()
+			if _, err := io.Copy(out, tarReader); err != nil {
+				log.Fatal(err)
+			}
+			noMatches = nil
+		}
+	}
+
+	return noMatches
+
+}
