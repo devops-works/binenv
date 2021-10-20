3af4342de0a89b8da880e6e98ff9e8adc726c4f6
Anton Patsev <10828883+patsevanton@users.noreply.github.com>
2021-10-20 11:22:36 +0600

    Change struct to Tgz

--- internal/install/tbz.go
+++ internal/install/tbz.go
@@ -1,70 +1,69 @@
 package install
 
 import (
-        "archive/tar"
-        "bytes"
-  "compress/bzip2"
-        "io"
-        "io/ioutil"
-        "log"
-        "os"
+	"archive/tar"
+	"bytes"
+	"compress/bzip2"
+	"io"
+	"io/ioutil"
+	"log"
+	"os"
 
-
-        "github.com/devops-works/binenv/internal/mapping"
-        "github.com/devops-works/binenv/internal/tpl"
+	"github.com/devops-works/binenv/internal/mapping"
+	"github.com/devops-works/binenv/internal/tpl"
 )
 
-// TarBZIP2 handles bzip2 files
-type TarBZIP2 struct {
-        filters []string
+// Tgz handles bzip2 files
+type Tgz struct {
+	filters []string
 }
 
 // Install file from bzip2 file
-func (x TarBZIP2) Install(src, dst, version string, mapper mapping.Mapper) error {
-        noMatches := ErrNoMatch
+func (x Tgz) Install(src, dst, version string, mapper mapping.Mapper) error {
+	noMatches := ErrNoMatch
 
-        data, err := ioutil.ReadFile(src)
-        if err != nil {
-                log.Fatal(err)
-        }
+	data, err := ioutil.ReadFile(src)
+	if err != nil {
+		log.Fatal(err)
+	}
 
-        r := bzip2.NewReader(bytes.NewReader(data))
+	r := bzip2.NewReader(bytes.NewReader(data))
 
-        tarReader := tar.NewReader(r)
-        args := tpl.New(version, mapper)
+	tarReader := tar.NewReader(r)
+	args := tpl.New(version, mapper)
 
-        for true {
-                header, err := tarReader.Next()
-                if err == io.EOF {
-                        break
-                }
+	for true {
+		header, err := tarReader.Next()
+		if err == io.EOF {
+			break
+		}
 
-                if err != nil {
-                        return err
-                }
+		if err != nil {
+			return err
+		}
 
-                switch header.Typeflag {
-                case tar.TypeReg: // regular file
-                        ok, err := args.MatchFilters(header.Name, x.filters)
-                        if err != nil {
-                                return err
-                        }
-                        if !ok {
-                                continue
-                        }
+		switch header.Typeflag {
+		case tar.TypeReg: // regular file
+			ok, err := args.MatchFilters(header.Name, x.filters)
+			if err != nil {
+				return err
+			}
+			if !ok {
+				continue
+			}
 
-                        out, err := os.OpenFile(dst, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0750)
-                        if err != nil {
-                                return err
-                        }
-                        defer out.Close()
-                        if _, err := io.Copy(out, tarReader); err != nil {
-                                log.Fatal(err)
-                        }
-                        noMatches = nil
-                }
-        }
+			out, err := os.OpenFile(dst, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0750)
+			if err != nil {
+				return err
+			}
+			defer out.Close()
+			if _, err := io.Copy(out, tarReader); err != nil {
+				log.Fatal(err)
+			}
+			noMatches = nil
+		}
+	}
 
-        return noMatches
+	return noMatches
 
 }
